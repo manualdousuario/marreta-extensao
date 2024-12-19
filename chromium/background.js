22718fec47677558f3bbaa4f28c2d26343bed196
Claromes <clarissamendes@alunos.utfpr.edu.br>
2024-12-19 02:03:37 -0300

    configura icones na barra superior

--- chromium/background.js
+++ chromium/background.js
@@ -1,13 +1,11 @@
 const MARRETA = "https://marreta.pcdomanual.com/p/";
+const PAGE_TITLE = "Abrir essa página com Marreta";
+const LINK_TITLE = "Abrir link com Marreta";
+const DISABLED_ICON = "./icon16-white.png";
+const ENABLED_ICON = "./icon16.png";
 
-// Address bar button
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    chrome.action.enable(tabId);
-  }
-});
-
-chrome.action.onClicked.addListener((tab) => {
+// Toolbar Button settings
+const toolbarEvent = (tab) => {
   chrome.scripting
     .executeScript({
       target: { tabId: tab.id },
@@ -15,22 +13,57 @@ chrome.action.onClicked.addListener((tab) => {
     })
     .then((results) => {
       const urlWithParam = `${MARRETA}${encodeURIComponent(results[0].result)}`;
-      chrome.tabs.create({ url: urlWithParam });
+      chrome.tabs.update({ url: urlWithParam });
     });
+};
+
+chrome.action.onClicked.addListener(toolbarEvent);
+
+const iconStatus = async (tabId) => {
+  try {
+    const tab = await chrome.tabs.get(tabId);
+
+    if (tab && tab.url) {
+      const isMarreta = tab.url.includes(MARRETA);
+      const iconPath = isMarreta ? ENABLED_ICON : DISABLED_ICON;
+      const title = isMarreta ? "" : PAGE_TITLE;
+
+      chrome.action.onClicked[isMarreta ? "removeListener" : "addListener"](
+        toolbarEvent
+      );
+
+      chrome.action.setIcon({ path: iconPath });
+      chrome.action.setTitle({ title });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Fires when the active tab in a window changes
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  iconStatus(activeInfo.tabId);
 });
 
-// Context menu
+// Fires when a tab/URL is updated
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.active) {
+    iconStatus(tabId);
+  }
+});
+
+// Context Menu settings
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: "sendTabUrl",
-    title: "Abrir essa página com Marreta",
+    title: PAGE_TITLE,
     contexts: ["page"],
     visible: true,
   });
 
   chrome.contextMenus.create({
     id: "sendLink",
-    title: "Abrir link com Marreta",
+    title: LINK_TITLE,
     contexts: ["link"],
     visible: true,
   });
@@ -39,7 +72,7 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "sendTabUrl" && tab.url) {
     const urlWithParam = `${MARRETA}${encodeURIComponent(tab.url)}`;
-    chrome.tabs.create({ url: urlWithParam });
+    chrome.tabs.update({ url: urlWithParam });
   } else if (info.menuItemId === "sendLink" && info.linkUrl) {
     const urlWithParam = `${MARRETA}${encodeURIComponent(info.linkUrl)}`;
     chrome.tabs.create({ url: urlWithParam });
--- firefox/background.js
+++ firefox/background.js
@@ -1,13 +1,11 @@
 const MARRETA = "https://marreta.pcdomanual.com/p/";
+const PAGE_TITLE = "Abrir essa página com Marreta";
+const LINK_TITLE = "Abrir link com Marreta";
+const DISABLED_ICON = "./icon16-white.png";
+const ENABLED_ICON = "./icon16.png";
 
-// Address bar button
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete") {
-    browser.pageAction.show(tabId);
-  }
-});
-
-browser.pageAction.onClicked.addListener((tab) => {
+// Address Bar Button settings
+const addressBarEvent = (tab) => {
   browser.scripting
     .executeScript({
       target: { tabId: tab.id },
@@ -15,22 +13,64 @@ browser.pageAction.onClicked.addListener((tab) => {
     })
     .then((results) => {
       const urlWithParam = `${MARRETA}${encodeURIComponent(results[0].result)}`;
-      browser.tabs.create({ url: urlWithParam });
+      browser.tabs.update({ url: urlWithParam });
     });
+};
+
+browser.pageAction.onClicked.addListener(addressBarEvent);
+
+const iconStatus = async (tabId) => {
+  try {
+    const tab = await browser.tabs.get(tabId);
+
+    if (tab && tab.url) {
+      const isMarreta = tab.url.includes(MARRETA);
+      const iconPath = isMarreta ? ENABLED_ICON : DISABLED_ICON;
+      const title = isMarreta ? "" : PAGE_TITLE;
+
+      browser.pageAction.onClicked[
+        isMarreta ? "removeListener" : "addListener"
+      ](addressBarEvent);
+
+      browser.pageAction.setIcon({
+        tabId: tabId,
+        path: {
+          16: iconPath,
+        },
+      });
+      browser.pageAction.setTitle({ tabId: tabId, title: title });
+
+      browser.pageAction.show(tabId);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Fires when the active tab in a window changes
+browser.tabs.onActivated.addListener((activeInfo) => {
+  iconStatus(activeInfo.tabId);
 });
 
-// Context menu
+// Fires when a tab/URL is updated
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.active) {
+    iconStatus(tabId);
+  }
+});
+
+// Context Menu settings
 browser.runtime.onInstalled.addListener(() => {
   browser.contextMenus.create({
     id: "sendTabUrl",
-    title: "Abrir essa página com Marreta",
+    title: PAGE_TITLE,
     contexts: ["page"],
     visible: true,
   });
 
   browser.contextMenus.create({
     id: "sendLink",
-    title: "Abrir link com Marreta",
+    title: LINK_TITLE,
     contexts: ["link"],
     visible: true,
   });
@@ -39,7 +79,7 @@ browser.runtime.onInstalled.addListener(() => {
 browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "sendTabUrl" && tab.url) {
     const urlWithParam = `${MARRETA}${encodeURIComponent(tab.url)}`;
-    browser.tabs.create({ url: urlWithParam });
+    browser.tabs.update({ url: urlWithParam });
   } else if (info.menuItemId === "sendLink" && info.linkUrl) {
     const urlWithParam = `${MARRETA}${encodeURIComponent(info.linkUrl)}`;
     browser.tabs.create({ url: urlWithParam });
